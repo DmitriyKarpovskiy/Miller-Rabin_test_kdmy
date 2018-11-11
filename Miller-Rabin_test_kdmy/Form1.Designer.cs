4ce025ca95cf2ea19c1393c4a98b3092a0289368
Дмитрий Карповский <dmkarpovskiy@gmail.com>
2018-11-11 15:49:12 +0200

    Добавьте файлы проекта.

---
+++ Miller-Rabin_test_kdmy/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Miller_Rabin_test_kdmy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_MillerRabin = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Go = new System.Windows.Forms.Button();
+            this.lb_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_MillerRabin
+            // 
+            this.lb_MillerRabin.AutoSize = true;
+            this.lb_MillerRabin.Font = new System.Drawing.Font("Tempus Sans ITC", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_MillerRabin.Location = new System.Drawing.Point(154, 9);
+            this.lb_MillerRabin.Name = "lb_MillerRabin";
+            this.lb_MillerRabin.Size = new System.Drawing.Size(143, 34);
+            this.lb_MillerRabin.TabIndex = 0;
+            this.lb_MillerRabin.Text = "Is it Simple?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // btn_Go
+            // 
+            this.btn_Go.Location = new System.Drawing.Point(178, 93);
+            this.btn_Go.Name = "btn_Go";
+            this.btn_Go.Size = new System.Drawing.Size(100, 35);
+            this.btn_Go.TabIndex = 2;
+            this.btn_Go.Text = "Go";
+            this.btn_Go.UseVisualStyleBackColor = true;
+            this.btn_Go.Click += new System.EventHandler(this.btn_Go_Click);
+            // 
+            // lb_result
+            // 
+            this.lb_result.AutoSize = true;
+            this.lb_result.Font = new System.Drawing.Font("Tempus Sans ITC", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_result.Location = new System.Drawing.Point(154, 189);
+            this.lb_result.Name = "lb_result";
+            this.lb_result.Size = new System.Drawing.Size(0, 35);
+            this.lb_result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 326);
+            this.Controls.Add(this.lb_result);
+            this.Controls.Add(this.btn_Go);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_MillerRabin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_MillerRabin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Go;
+        private System.Windows.Forms.Label lb_result;
+    }
+}
+
---
+++ Miller-Rabin_test_kdmy/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Numerics;
+
+namespace Miller_Rabin_test_kdmy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MinimumSize = new Size(this.Width, this.Height); //no smaller than design size
+            this.MaximumSize = new Size(this.Width, this.Height); //no bigger than design size
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) //Text-box for a number allows to enter only digits
+            {
+                e.Handled = true;
+            }
+        }
+
+        public bool MillerRabinTest(BigInteger n, int k)
+        {
+            // if n == 2 or n == 3 - 'simple', return true
+            if (n == 2 || n == 3)
+                return true;
+
+            // if n < 2 or n is eve - false
+            if (n < 2 || n % 2 == 0)
+                return false;
+
+            // show n − 1 as (2^s)·t, where t is not even
+            BigInteger t = n - 1;
+
+            int s = 0;
+
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s += 1;
+            }
+
+            // repeat k times
+            for (int i = 0; i < k; i++)
+            {
+                // select random nunber a in [2, n − 2]
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+                byte[] _a = new byte[n.ToByteArray().LongLength];
+
+                BigInteger a;
+
+                do
+                {
+                    rng.GetBytes(_a);
+                    a = new BigInteger(_a);
+                }
+                while (a < 2 || a >= n - 2);
+
+                // x ← a^t mod n
+                BigInteger x = BigInteger.ModPow(a, t, n);
+
+                // if x == 1 ore x == n − 1, next iteration
+                if (x == 1 || x == n - 1)
+                    continue;
+
+                // repeat s − 1 times
+                for (int r = 1; r < s; r++)
+                {
+                    // x ← x^2 mod n
+                    x = BigInteger.ModPow(x, 2, n);
+
+                    // if x == 1, return "composite"
+                    if (x == 1)
+                        return false;
+
+                    // if x == n − 1, next iteration of outer loop
+                    if (x == n - 1)
+                        break;
+                }
+
+                if (x != n - 1)
+                    return false;
+            }
+
+            // return "simple"
+            return true;
+        }
+
+        private void btn_Go_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BigInteger n = Convert.ToInt64(textBox1.Text);
+                bool res = MillerRabinTest(n, 10);
+                if (res)
+                {
+                    lb_result.Text = "Is simple";
+                }
+                else
+                {
+                    lb_result.Text = "Is NOT simple";
+                }
+            }
+            catch
+            {
+                lb_result.Text = "Something goes wrong";
+            }
+        }
+    }
+}
